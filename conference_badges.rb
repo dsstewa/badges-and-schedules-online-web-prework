3100c0dab4e4c75645f60baa13c376b82f73ba78
David Stewart <davidstewart@gmail.com>
2019-02-17 04:02:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+
+nametest = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  names = []
+  array.each do |name|
+    names.push(badge_maker(name))
+end
+   return names
+end
+
+def assign_rooms(array)
+rooms = []
+array.each_index do |x|
+  rooms.push("Hello, #{array[x]}! You'll be assigned to room #{x + 1}!")
+end
+return rooms
+end
+  
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+end
+  
+  
+
+
+
+
+
+require 'pry'
+
+binding.pry
